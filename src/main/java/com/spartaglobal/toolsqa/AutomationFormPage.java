1a9e4325a43a79a9625478cf0aec1ed88a639df0
T L Goddard <TGoddard@spartaglobal.com>
2019-05-02 09:04:56 +0100

    all done apart from last 2 boxes

--- src/main/java/com/spartaglobal/toolsqa/AutomationFormPage.java
+++ src/main/java/com/spartaglobal/toolsqa/AutomationFormPage.java
@@ -16,16 +16,29 @@ public class AutomationFormPage {
     private final By firstName = By.xpath("//*[@id=\"content\"]/div[1]/div/div/div/div[2]/div/form/fieldset/div[8]/input");
     private final By lastName = By.xpath("//*[@id=\"content\"]/div[1]/div/div/div/div[2]/div/form/fieldset/div[11]/input");
     private final By sexElements = By.name("sex");
+    private final By experianceElements = By.name("exp");
+    private final By date = By.id("datepicker");
+    private final By professionElements = By.name("profession");
+    private final By toolElements = By.name("tool");
+    private final By hybridFrameworkDownload = By.linkText("Selenium Automation Hybrid Framework");
+    private final By testFileDownload = By.linkText("Test File to Download");
     private final String practiceFormURL = "https://www.toolsqa.com/automation-practice-form/";
+    private final String downLoadURL = "http://toolsqa.com/wp-content/uploads/2014/04/OnlineStore.zip";
+
+
     private List<String> sexRadioButtonElements;
+    private List<String> experianceRadioButtonElements;
+    private List<String> toolButtonElements;
+    private List<String> professionButtonElements;
 
 
     public AutomationFormPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public WebElement findElement(By element){
-        return driver.findElement(element);}
+    public WebElement findElement(By element) {
+        return driver.findElement(element);
+    }
 
     public AutomationFormPage goToHomePage() {
         driver.navigate().to(practiceFormURL);
@@ -36,11 +49,11 @@ public class AutomationFormPage {
         return findElement(partialLinkTest).isDisplayed();
     }
 
-    public String partialLinkTestText(){
+    public String partialLinkTestText() {
         return findElement(partialLinkTest).getText();
     }
 
-    public String getCurrentURL(){
+    public String getCurrentURL() {
         return driver.getCurrentUrl();
     }
 
@@ -50,42 +63,42 @@ public class AutomationFormPage {
         return this;
     }
 
-    public AutomationFormPage linkTestClick(){
+    public AutomationFormPage linkTestClick() {
         findElement(linkTest).click();
         return this;
     }
 
-    public boolean linkTableDisplayed(){
+    public boolean linkTableDisplayed() {
         return findElement(linkTable).isDisplayed();
     }
 
-    public AutomationFormPage inputFirstName(String first){
+    public AutomationFormPage inputFirstName(String first) {
         findElement(firstName).sendKeys(first);
         return this;
     }
 
-    public boolean firstNameDisplayed(){
+    public boolean firstNameDisplayed() {
         return findElement(firstName).isDisplayed();
     }
 
-    public String getDisplayedFirstName(){
+    public String getDisplayedFirstName() {
         return findElement(firstName).getAttribute("value");
     }
 
-    public AutomationFormPage inputLastName(String first){
+    public AutomationFormPage inputLastName(String first) {
         findElement(lastName).sendKeys(first);
         return this;
     }
 
-    public boolean lastNameDisplayed(){
+    public boolean lastNameDisplayed() {
         return findElement(lastName).isDisplayed();
     }
 
-    public String getDisplayedLastName(){
+    public String getDisplayedLastName() {
         return findElement(lastName).getAttribute("value");
     }
 
-    public List<WebElement> findElements(By element){
+    public List<WebElement> findElements(By element) {
         return driver.findElements(element);
     }
 
@@ -102,36 +115,188 @@ public class AutomationFormPage {
 //        return isDisplayed;
 //    }
 
-    private void getSexElements(){
+    private void getSexElements() {
         List<String> sexRadioBElements = new ArrayList<>();  //creates a new string arraylist
 
         for (WebElement sexElement : driver.findElements(sexElements)) {  //find elements make a list of button elements and uses By.name("sex") to get them all since this is the heading they have in common.
             sexRadioBElements.add(sexElement.getAttribute("value")); //adds the string value i.e. "male and "female" to the newly created arraylist corresponding to each button
         }
 
-        sexRadioButtonElements =  sexRadioBElements; //assigns the created liststring to the memeber variable of the class
+        sexRadioButtonElements = sexRadioBElements; //assigns the created liststring to the memeber variable of the class
     }
 
-    public void selectSex(String sexSelected) { //sex selected is the button whose corresponding value that you want to click on
-        if (sexRadioButtonElements == null) { getSexElements(); }  //creates the string array using above private method
+    public AutomationFormPage selectSex(String sexSelected) { //sex selected is the button whose corresponding value that you want to click on
+        if (sexRadioButtonElements == null) {
+            getSexElements();
+        }  //creates the string array using above private method
 
-        if (sexRadioButtonElements.contains(sexSelected)){ //checks to see if the new liststring contains the value you want to accept
+        if (sexRadioButtonElements.contains(sexSelected)) { //checks to see if the new liststring contains the value you want to accept
             driver.findElement(By.id("sex-" + sexRadioButtonElements.indexOf(sexSelected))).click(); //if true append the index of the position in the arraylist of the selected button in order to click the right button
         } else {
             throw new IllegalArgumentException("The requested element was not available. Please use one of the following " + sexRadioButtonElements); // if the button valiue is not in the string it will throw an exception and give the list of valid sex to select in the call
         }
+        return this;
     }
 
-    public boolean checkSelectedSexButton(String selectedSex){
-        boolean isSexSelected;
-        if (sexRadioButtonElements == null) { getSexElements(); } //same as above
+    public boolean checkSelectedSexButton(String selectedSex) {
+        if (sexRadioButtonElements == null) {
+            getSexElements();
+        } //same as above
 
-        if (sexRadioButtonElements.contains(selectedSex)){ //same as above
-            isSexSelected = driver.findElement(By.id("sex-" + sexRadioButtonElements.indexOf(selectedSex))).isSelected(); // checks to see if the selected button is selected
+        if (sexRadioButtonElements.contains(selectedSex)) { //same as above
+            driver.findElement(By.id("sex-" + sexRadioButtonElements.indexOf(selectedSex))).isSelected(); // checks to see if the selected button is selected
         } else {
             throw new IllegalArgumentException("The requested element was not selected. Please use one of the following " + sexRadioButtonElements + " & that it hs been successfully selected");
         }
-        return isSexSelected;
+        return driver.findElement(By.id("sex-" + sexRadioButtonElements.indexOf(selectedSex))).isSelected();
     }
 
+    private void getExperianceElements() {
+        List<String> ExperianceRadioBElements = new ArrayList<>();
+
+        for (WebElement experianceElement : driver.findElements(experianceElements)) {
+            ExperianceRadioBElements.add(experianceElement.getAttribute("value"));
+        }
+        experianceRadioButtonElements = ExperianceRadioBElements;
+    }
+
+    public AutomationFormPage selectExperianceButton(String selectedNumber) {
+        if (experianceRadioButtonElements == null) {
+            getExperianceElements();
+        }
+        if (experianceRadioButtonElements.contains(selectedNumber)) {
+            driver.findElement(By.id("exp-" + experianceRadioButtonElements.indexOf(selectedNumber))).click();
+        } else {
+            throw new IllegalArgumentException("The requested element was not available. Please use one of the following " + experianceRadioButtonElements);
+        }
+        return this;
+
+    }
+
+    public boolean checkExperianceButton(String selectedNumber) {
+        if (experianceRadioButtonElements == null) {
+            getExperianceElements();
+        }
+        if (experianceRadioButtonElements.contains(selectedNumber)) {
+            driver.findElement(By.id("exp-" + experianceRadioButtonElements.indexOf(selectedNumber))).isSelected();
+        } else {
+            throw new IllegalArgumentException("The requested element was not available. Please use one of the following " + experianceRadioButtonElements);
+        }
+        return driver.findElement(By.id("exp-" + experianceRadioButtonElements.indexOf(selectedNumber))).isSelected();
+    }
+
+    public AutomationFormPage inputDate(String inputDate) {
+        findElement(date).sendKeys(inputDate);
+        return this;
+    }
+
+    public boolean DateDisplayed() {
+        return findElement(date).isDisplayed();
+    }
+
+    public String getDisplayedDate() {
+        return findElement(date).getAttribute("value");
+    }
+
+
+    private void getProfessionElements() {
+        List<String> professionRadioBElements = new ArrayList<>();
+
+        for (WebElement professionElement : driver.findElements(professionElements)) {
+            professionRadioBElements.add(professionElement.getAttribute("value"));
+        }
+        professionButtonElements = professionRadioBElements;
+    }
+
+    public AutomationFormPage selectProfessionButton(String selectedProfession) {
+        if (professionButtonElements == null) {
+            getProfessionElements();
+        }
+        if (professionButtonElements.contains(selectedProfession)) {
+            driver.findElement(By.id("profession-" + professionButtonElements.indexOf(selectedProfession))).click();
+        } else {
+            throw new IllegalArgumentException("The requested element was not available. Please use one of the following " + professionButtonElements);
+        }
+        return this;
+    }
+
+    public boolean checkprofessionButton (String selectedProfession){
+        if (professionButtonElements == null) {
+            getProfessionElements();
+        }
+        if (professionButtonElements.contains(selectedProfession)) {
+            driver.findElement(By.id("profession-" + professionButtonElements.indexOf(selectedProfession))).isSelected();
+        } else {
+            throw new IllegalArgumentException("The requested element was not available. Please use one of the following " + professionButtonElements);
+        }
+        return driver.findElement(By.id("profession-" + professionButtonElements.indexOf(selectedProfession))).isSelected();
+    }
+
+    private void getToolElements() {
+        List<String> toolRadioBElements = new ArrayList<>();
+
+        for (WebElement toolElement : driver.findElements(toolElements)) {
+            toolRadioBElements.add(toolElement.getAttribute("value"));
+        }
+        toolButtonElements = toolRadioBElements;
+    }
+
+    public AutomationFormPage selecttoolButton(String selectedTool) {
+        if (toolButtonElements == null) {
+            getToolElements();
+        }
+        if (toolButtonElements.contains(selectedTool)) {
+            driver.findElement(By.id("tool-" + toolButtonElements.indexOf(selectedTool))).click();
+        } else {
+            throw new IllegalArgumentException("The requested element was not available. Please use one of the following " + toolButtonElements);
+        }
+        return this;
+    }
+
+    public boolean checktoolButton (String selectedTool){
+        if (toolButtonElements == null) {
+            getToolElements();
+        }
+        if (toolButtonElements.contains(selectedTool)) {
+            driver.findElement(By.id("tool-" + toolButtonElements.indexOf(selectedTool))).isSelected();
+        } else {
+            throw new IllegalArgumentException("The requested element was not available. Please use one of the following " + toolButtonElements);
+        }
+        return driver.findElement(By.id("tool-" + toolButtonElements.indexOf(selectedTool))).isSelected();
+    }
+
+    public boolean seleniumAutomationHybridFrameworkDisplayed(){
+        return driver.findElement(hybridFrameworkDownload).isDisplayed();
+    }
+
+    public String seleniumAutomationHybridFrameworkDisplayedTextTest(){
+        return driver.findElement(hybridFrameworkDownload).getText();
+    }
+
+    public AutomationFormPage HybridFrameworkClick(){
+        findElement(hybridFrameworkDownload).click();
+        return this;
+    }
+
+    public AutomationFormPage goToDownLoadPage(){
+        driver.navigate().to(downLoadURL);
+        return this;
+    }
+    public String getHybridFrameWorkURLAttribute(){
+        return driver.findElement(hybridFrameworkDownload).getAttribute("href");
+    }
+
+    public boolean seleniumAutomationTestFileDisplayed(){
+        return driver.findElement(testFileDownload).isDisplayed();
+    }
+
+    public String seleniumAutomationTestFileDisplayedTextTest(){
+        return driver.findElement(testFileDownload).getText();
+    }
+
+    public String getTestFileDisplayURLAttribute(){
+        return driver.findElement(testFileDownload).getAttribute("href");
+    }
 }
+
+
--- src/test/java/com/spartaglobal/toolsqa/FormPageTest.java
+++ src/test/java/com/spartaglobal/toolsqa/FormPageTest.java
@@ -73,19 +73,147 @@ public class FormPageTest {
         Assert.assertEquals(toolsWebsite.AutomationFormPage().goToHomePage().inputLastName(text).getDisplayedLastName(), text);
     }
 
+//    @Test
+//    public void isSizeOfSexList(){
+//        setup();
+//        int size = 2;
+//        Assert.assertEquals(toolsWebsite.AutomationFormPage().goToHomePage().sexessize(),2);
+//    }
+//
+//    @Test
+//    public void isSexButtonsDisplayed(){
+//        setup();
+//        Boolean [] truthArray = new Boolean[] {true, true};
+//        Assert.assertArrayEquals(toolsWebsite.AutomationFormPage().goToHomePage().sexesButtonsDisplayed(), truthArray);
+//    }
+
     @Test
-    public void isSizeOfSexList(){
+    public void isSexButtonSelected(){
         setup();
-        int size = 2;
-        Assert.assertEquals(toolsWebsite.AutomationFormPage().goToHomePage().sexessize(),2);
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().goToHomePage().selectSex("Male").checkSelectedSexButton("Male"));
     }
 
     @Test
-    public void isSexButtonsDisplayed(){
+    public void isAfterSelectingOneButtonClickOntheOtherIsThatSelectedAndTheOtherOneNotSelected(){
         setup();
-        Boolean [] truthArray = new Boolean[] {true, true};
-        Assert.assertArrayEquals(toolsWebsite.AutomationFormPage().goToHomePage().sexesButtonsDisplayed(), truthArray);
+        toolsWebsite.AutomationFormPage().goToHomePage().selectSex("Male");
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().goToHomePage().selectSex("Female").checkSelectedSexButton("Female"));
+        Assert.assertFalse(toolsWebsite.AutomationFormPage().goToHomePage().checkSelectedSexButton("Male"));
     }
+
+    @Test
+    public void isExperianceButtonSelected(){
+        setup();
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().goToHomePage().selectExperianceButton("5").checkExperianceButton("5"));
+    }
+
+    @Test
+    public void isDateDisplayed(){
+        setup();
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().goToHomePage().DateDisplayed());
+    }
+
+    @Test
+    public void isDateTextDisplayed(){
+        setup();
+        String date = "12/12/2012";
+        Assert.assertEquals(toolsWebsite.AutomationFormPage().goToHomePage().inputDate(date).getDisplayedDate(), date);
+    }
+
+    @Test
+    public void isBothProfessionButtonsSelected(){
+        setup();
+        toolsWebsite.AutomationFormPage().goToHomePage().selectProfessionButton("Manual Tester").selectProfessionButton("Automation Tester");
+
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().checkprofessionButton("Manual Tester") && toolsWebsite.AutomationFormPage().checkprofessionButton("Automation Tester"));
+    }
+
+    @Test
+    public void isManualProfessionButtonSelected(){
+        setup();
+        toolsWebsite.AutomationFormPage().goToHomePage().selectProfessionButton("Manual Tester");
+
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().checkprofessionButton("Manual Tester"));
+    }
+
+    @Test
+    public void isAutomaticProfessionButtonSelected(){
+        setup();
+        toolsWebsite.AutomationFormPage().goToHomePage().selectProfessionButton("Automation Tester");
+
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().checkprofessionButton("Automation Tester"));
+    }
+
+    @Test
+    public void isAllThreeToolButtonsSelected(){
+        setup();
+        toolsWebsite.AutomationFormPage().goToHomePage().selecttoolButton("QTP").selecttoolButton("Selenium IDE").selecttoolButton("Selenium Webdriver");
+
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().checktoolButton("QTP") && toolsWebsite.AutomationFormPage().checktoolButton("Selenium IDE")&& toolsWebsite.AutomationFormPage().checktoolButton("Selenium Webdriver"));
+    }
+
+    @Test
+    public void isTwoToolButtonsSelected(){
+        setup();
+        toolsWebsite.AutomationFormPage().goToHomePage().selecttoolButton("QTP").selecttoolButton("Selenium IDE");
+
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().checktoolButton("QTP") && toolsWebsite.AutomationFormPage().checktoolButton("Selenium IDE"));
+    }
+
+    @Test
+    public void isQTPToolButtonsSelected(){
+        setup();
+        toolsWebsite.AutomationFormPage().goToHomePage().selecttoolButton("QTP");
+
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().checktoolButton("QTP"));
+    }
+
+    @Test
+    public void isHybridFrameWorkDisplayed(){
+        setup();
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().goToHomePage().seleniumAutomationHybridFrameworkDisplayed());
+    }
+
+    @Test
+    public void isHybridFrameWorkTextDisplayed(){
+        setup();
+        Assert.assertEquals(toolsWebsite.AutomationFormPage().goToHomePage().seleniumAutomationHybridFrameworkDisplayedTextTest(), "Selenium Automation Hybrid Framework");
+    }
+
+    @Test
+    public void checkHybridFrameWorkURL(){
+        setup();
+        Assert.assertEquals(toolsWebsite.AutomationFormPage().goToHomePage().getHybridFrameWorkURLAttribute(), "http://toolsqa.com/wp-content/uploads/2014/04/OnlineStore.zip");
+    }
+
+    @Test
+    public void isTestFileDisplayed(){
+        setup();
+        Assert.assertTrue(toolsWebsite.AutomationFormPage().goToHomePage().seleniumAutomationTestFileDisplayed());
+    }
+
+    @Test
+    public void isTestFileTextDisplayed(){
+        setup();
+        Assert.assertEquals(toolsWebsite.AutomationFormPage().goToHomePage().seleniumAutomationTestFileDisplayedTextTest(), "Test File to Download");
+    }
+
+    @Test
+    public void checkTestFileURL(){
+        setup();
+        Assert.assertEquals(toolsWebsite.AutomationFormPage().goToHomePage().getTestFileDisplayURLAttribute(), "http://toolsqa.com/wp-content/uploads/2016/09/Test-File-to-Download.xlsx");
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
